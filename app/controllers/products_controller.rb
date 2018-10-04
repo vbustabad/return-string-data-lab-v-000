fe924b985a7a663f439bfdc30e0f9522ec0c78f4
Veronica Bustabad <vbustabad@gmail.com>
2018-10-04 17:51:28 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,13 @@
 class ProductsController < ApplicationController
 
   def description
-    product = Product.find(params[:id])
-    render plain: product.description
+    @product = Product.find(params[:id])
+    render plain: @product.description
+  end
+
+  def inventory
+    @product = Product.find(params[:id])
+    render plain: @product.inventory
   end
 
   def index
