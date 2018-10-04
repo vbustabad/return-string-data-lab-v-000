240359877d1a674b6bdd7ae99a74d376864e5e65
Veronica Bustabad <vbustabad@gmail.com>
2018-10-04 18:08:24 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -25,7 +25,7 @@ class ProductsController < ApplicationController
   def create
     @product = Product.new(product_params)
     @product.save
-    redirect_to product_path(@product)
+    redirect_to products_path
   end
 
   def show
