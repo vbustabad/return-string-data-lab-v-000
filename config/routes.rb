d0671237c5d5c97c77dd251d6e099d2b8c1705d1
Veronica Bustabad <vbustabad@gmail.com>
2018-10-04 17:47:45 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
   get 'home', to: 'static#home'
   resources :orders
   resources :invoices
+  resources :products
   get '/products/:id/description', to: 'products#description'
-  get '/products', to: 'products#index'
-  post '/products', to: 'products#create'
-  get '/products/:id', to: 'products#show'
 end
